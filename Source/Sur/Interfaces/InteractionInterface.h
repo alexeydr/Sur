52e833509ad43400e66675277d31720a30fea3f5
alexeydr <55873949+alexeydr@users.noreply.github.com>
2021-01-14 03:09:06 +0300

    Components

---
+++ Source/Sur/Interfaces/InteractionInterface.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "InteractionInterface.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUR_API UInteractionInterface : public UObject
+{
+	GENERATED_BODY()
+	
+};
